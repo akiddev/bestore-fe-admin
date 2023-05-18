66f9a3a78f1b1db957cedfa2c491b74d867c255e
akiddev <npgbao170@gmail.com>
2023-05-18 11:30:34 +0700

    add insert category functions

--- src/components/categories/AddOrEditCategory.jsx
+++ src/components/categories/AddOrEditCategory.jsx
@@ -1,12 +1,18 @@
-import { PageHeader } from "@ant-design/pro-layout";
 import React, { Component } from "react";
 import withRouter from "../../helpers/withRouter";
 import { Button, Col, Divider, Form, Input, Row, Select } from "antd";
 import ContentHeader from "../common/ContentHeader";
+import { insertCategory } from "./../../redux/actions/categoryAction";
+import { connect } from "react-redux";
+import categoryReducer from "./../../redux/reducers/categoryReducer";
 
 class AddOrEditCategory extends Component {
   onSubmitForm = (values) => {
     console.log(values);
+
+    const { navigate } = this.props.router;
+
+    this.props.insertCategory(values, navigate);
   };
 
   render() {
@@ -55,4 +61,14 @@ class AddOrEditCategory extends Component {
   }
 }
 
-export default withRouter(AddOrEditCategory);
+const mapStateToProps = (state) => ({
+  category: state.categoryReducer.category,
+});
+
+const mapDispatchToProps = {
+  insertCategory,
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(AddOrEditCategory)
+);
--- src/redux/actions/categoryAction.jsx
+++ src/redux/actions/categoryAction.jsx
@@ -1,9 +1,23 @@
+import CategoryService from "../../services/categoryService";
+import { CATEGORY_SET } from "./actionTypes";
+
 //Insert Category
 export const insertCategory = (category, navigate) => async (dispatch) => {
-  // const service = CategoryService()
+  const service = new CategoryService();
 
   try {
     console.log("insert category");
+
+    const response = await service.insertCategory(category);
+
+    if(response.status === 201){
+      dispatch({
+        type : CATEGORY_SET,
+        payload : response.data
+      })
+    }
+
+    console.log(response);
   } catch (error) {
     console.log("Error" + error);
   }
